8c83ff37d75890d4dbc6025ed337ef46b4680d94
Cornel de Jager <cornel.jager@gmail.com>
2019-03-16 08:39:58 +1000

    Updated DateTime Fields Commit

--- Assignment_1/DMProj1.py
+++ Assignment_1/DMProj1.py
@@ -8,7 +8,7 @@ Created on Mon Mar  4 17:29:50 2019
 # Imports
 import pandas as pd
 import numpy as np
-
+import datetime 
 # Ignore Warnings
 import warnings
 warnings.filterwarnings("ignore")
@@ -20,37 +20,55 @@ WheelTypeMapping = {'Alloy':1,  'Covers':2, 'Special':3}
 # Do the data analysis
 data = pd.read_csv("Kick.csv", na_values = '?')
 
-def PreProcessing (data):
+def PreProcessing (df):
     print("Pre-Processing Step")
     
     # Handle Bad Columns drop Columns
     print("Drop PRIMEUNIT due to incufficient data amount")
     print("Drop AUCGUART due to incufficient data amount and is leaky data")
     print("Drop wheel type due to it being a dupplicate of wheelType ID.")
-    data.drop(['PRIMEUNIT', 'AUCGUART', 'WheelTypeID'], axis=1, inplace=True)
+    print("Drop Purchase date as it is a duplicate of time stamp")
+    
+    df.drop(['PRIMEUNIT', 'AUCGUART', 'WheelTypeID', 'PurchaseDate'], axis=1, inplace=True)
     
     # Handle Missing Values
     i = 0            # Python's indexing starts at zero
     for item in data['TopThreeAmericanName']:   # Python's for loops are a "for each" loop 
-        if data['TopThreeAmericanName'][i] == np.nan and  data['Make'][i] == 'Hyundai':
-            data['TopThreeAmericanName'][i] = 'HYUNDAI'
+        if df['TopThreeAmericanName'][i] == np.nan and  df['Make'][i] == 'Hyundai':
+            df['TopThreeAmericanName'][i] = 'HYUNDAI'
         i += 1
         
     i = 0            # Python's indexing starts at zero
     for item in data['TopThreeAmericanName']:   # Python's for loops are a "for each" loop 
-        if data['TopThreeAmericanName'][i] == np.nan and  data['Make'][i] == 'Jeep':
-            data['TopThreeAmericanName'][i] = 'JEEP'
+        if df['TopThreeAmericanName'][i] == np.nan and  df['Make'][i] == 'Jeep':
+            df['TopThreeAmericanName'][i] = 'JEEP'
         i += 1
+        
+    # Change time stamp integer to datetime
+    print("Convert timestamp to datetime")
+    df['PurchaseTimestamp'] = pd.to_datetime(data['PurchaseTimestamp'], unit='s')
     
-    print(data.groupby(['TopThreeAmericanName'])['Make'].value_counts())
+    # create a new weekday column
+    df['Weekday'] = np.nan
+     
+    for i in range(1,len(df['PurchaseTimestamp'])):
+        df['Weekday'] = df['PurchaseTimestamp'][i].weekday()
+        
+    # remove the rows where the cars are not for sale
+    df = df[df.ForSale == 'No']
+    df = df[df.ForSale == '0']
     
-def Question1(data):
+    # now remove the ForSale column    
+    df.drop(['ForSale'], axis=1, inplace=True)
+    return df
+
+def Question1(df):
     print("Question 1.")
     
     # check to see if the are any odd labaels
-    
-    counts = data["IsBadBuy"].value_counts()
-    total = data["IsBadBuy"].count()
+    print(df)
+    counts = df["IsBadBuy"].value_counts()
+    total = df["IsBadBuy"].count()
     
     # calculate the percentage
     # kick is where IsBadBuy == 1
