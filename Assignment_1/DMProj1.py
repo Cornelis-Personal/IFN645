ccbfac625a1fe3022335218353fc3bc5cf3920a1
Cornelis De Jager <n8891974@qut.edu.au>
2019-03-18 18:46:25 +1000

    Fix master

--- Assignment_1/DMProj1.py
+++ Assignment_1/DMProj1.py
@@ -38,7 +38,7 @@ def PreProcessing (df):
     print("Pre-Processing Step")
     
     # Check if there are any missing target variables
-    if data['IsBadBuy'].isnull().values.any() == True:
+    if df['IsBadBuy'].isnull().values.any() == True:
         print("Missing Target Variables")
     else:
         print("No missing Target Variables")
@@ -50,27 +50,27 @@ def PreProcessing (df):
     print("Drop WheelTypeID due to it being a duplicate of WheelType")
     print("Drop ForSale due to data skew")
     print("Drop PurchaseDate due to it being a duplicate of PurchaseTimeStamp")
-    data.drop(['PRIMEUNIT', 'AUCGUART', 'WheelTypeID', 'PurchaseDate'], 
+    df.drop(['PRIMEUNIT', 'AUCGUART', 'WheelTypeID', 'PurchaseDate'], 
               axis=1, 
               inplace=True)
     
     
     # Handle Missing Values
     i = 0            # Python's indexing starts at zero
-    for item in data['TopThreeAmericanName']:   # Python's for loops are a "for each" loop 
+    for item in df['TopThreeAmericanName']:   # Python's for loops are a "for each" loop 
         if df['TopThreeAmericanName'][i] == np.nan and  df['Make'][i] == 'Hyundai':
             df['TopThreeAmericanName'][i] = 'HYUNDAI'
         i += 1
         
     i = 0            # Python's indexing starts at zero
-    for item in data['TopThreeAmericanName']:   # Python's for loops are a "for each" loop 
+    for item in df['TopThreeAmericanName']:   # Python's for loops are a "for each" loop 
         if df['TopThreeAmericanName'][i] == np.nan and  df['Make'][i] == 'Jeep':
             df['TopThreeAmericanName'][i] = 'JEEP'
         i += 1
         
         # Change time stamp integer to datetime
     print("Convert timestamp to datetime")
-    df['PurchaseTimestamp'] = pd.to_datetime(data['PurchaseTimestamp'], unit='s')
+    df['PurchaseTimestamp'] = pd.to_datetime(df['PurchaseTimestamp'], unit='s')
     
     # create a new weekday column
     df['Weekday'] = np.nan
@@ -90,20 +90,20 @@ def PreProcessing (df):
     """ DOES THIS HAVE TO BE THERE """
     
     # Standardise the capitilization across all object rows
-    obj_cols = data.select_dtypes(include='object').columns # Create a list of col names
+    obj_cols = df.select_dtypes(include='object').columns # Create a list of col names
     for i in obj_cols:              # Interate over the obj_cols list
-        data[i] = data[i].str.upper()   # Convert all strings to uppercase
+        df[i] = df[i].str.upper()   # Convert all strings to uppercase
         
     # Standardize USA to AMERICA
-    data['Nationality'].replace({'USA' : 'AMERICAN'}, inplace = True)
+    df['Nationality'].replace({'USA' : 'AMERICAN'}, inplace = True)
     
     # Turn Transmission into a binary variable with Auto = 1 and Manual = 0
-    data.rename({'Transmission' : 'Auto'}, axis = 1, inplace = True)
+    df.rename({'Transmission' : 'Auto'}, axis = 1, inplace = True)
     # Replace binary columns with 1s and 0s
-    data['Auto'].replace({'MANUAL' : 0, 'AUTO' : 1}, inplace=True)
+    df['Auto'].replace({'MANUAL' : 0, 'AUTO' : 1}, inplace=True)
     
     # Remove NOT AVAIL in color and place it in the NaN section
-    data['Color'].replace({'NOT AVAIL': np.nan}, inplace = True)
+    df['Color'].replace({'NOT AVAIL': np.nan}, inplace = True)
     
     
     """ I'll have to check with teach if this is correct
@@ -120,92 +120,80 @@ def PreProcessing (df):
     print("The pValaue is " + str(pValue) + " which is significant enough to reject null hypothesis")
     """
     print("Drop VNST due to statistical insignificance")
-    data.drop('VNST', axis=1, inplace = True)
+    df.drop('VNST', axis=1, inplace = True)
     
     
     # Seperate the Size feature into Size and Body
-    tempSize = data['Size'].str.split(' ', expand = True) # Create temp var with split column
-    data['Size'] = tempSize[0] # Save the temp var back into data
-    data['Body'] = tempSize[1] # Save the temp var back into data
-    data['Body'].fillna('CITY', inplace = True) # Assume any other cars are 'City'
-    data.loc[data.Size == 'VAN', 'Body'] = 'Van' # Convert Van into a body type
-    data.loc[data.Size == 'VAN', 'Size'] =  np.nan # Take van away from size, shouldn't matter once OH is done
+    tempSize = df['Size'].str.split(' ', expand = True) # Create temp var with split column
+    df['Size'] = tempSize[0] # Save the temp var back into data
+    df['Body'] = tempSize[1] # Save the temp var back into data
+    df['Body'].fillna('CITY', inplace = True) # Assume any other cars are 'City'
+    df.loc[df.Size == 'VAN', 'Body'] = 'Van' # Convert Van into a body type
+    df.loc[df.Size == 'VAN', 'Size'] =  np.nan # Take van away from size, shouldn't matter once OH is done
     
     
     # Replace all non 0, 1 values in IsOnlineSale to 1
-    maskOnlineSale = data['IsOnlineSale'] != 0 # Any value that isn't 0 will be set to 1
-    data.loc[maskOnlineSale, 'IsOnlineSale'] = 1 # Set the values to 1
+    maskOnlineSale = df['IsOnlineSale'] != 0 # Any value that isn't 0 will be set to 1
+    df.loc[maskOnlineSale, 'IsOnlineSale'] = 1 # Set the values to 1
     
     
     # Converting the TimeStamp into Quater
     Quater = [] # Create empty string
-    for i, _ in enumerate(data.PurchaseTimestamp): # Loop over the entire dataset
+    for i, _ in enumerate(df.PurchaseTimestamp): # Loop over the entire dataset
         # Convert the epoch datetime into the quater and append to list
-        Quater.append(pd.Timestamp(data.PurchaseTimestamp.loc[i], unit = 's').quarter)        
-    data['Quater'] = Quater # Create the column with list
-    data.drop('PurchaseTimestamp', axis=1, inplace = True) # Drop old TimeStamp
+        Quater.append(pd.Timestamp(df.PurchaseTimestamp.loc[i], unit = 's').quarter)        
+    df['Quater'] = Quater # Create the column with list
+    df.drop('PurchaseTimestamp', axis=1, inplace = True) # Drop old TimeStamp
     
     
     
     """ This will take care of any Null values we don't specifically take care of
     by replaceing the missing data with data from the same distibution"""
-    for i in data.columns: # Loop over dataset
-        if data[i].isna().any(): # Check to see if there is a NaN is the feature
-            dist = data[i].value_counts(normalize=True) # Find the distrabution of the column
-            missing = data[i].isna() # Find where the NaN are
+    for i in df.columns: # Loop over dataset
+        if df[i].isna().any(): # Check to see if there is a NaN is the feature
+            dist = df[i].value_counts(normalize=True) # Find the distrabution of the column
+            missing = df[i].isna() # Find where the NaN are
             # Replace the NaNs with values from the same distrabution of the column
-            data.loc[missing, i] = np.random.choice(dist.index, size=len(data[missing]),p=dist.values)       
+            df.loc[missing, i] = np.random.choice(dist.index, size=len(df[missing]),p=dist.values)       
             print("Converted all of " + i + "s missing values into the same distrubution")
     
     
     """ This should be the last thing done """
     # Convert all categorical variables into one hot representations
     
-    print("The number of features before one hot encoding is " + str(data.shape[1]))
+    print("The number of features before one hot encoding is " + str(df.shape[1]))
     data_OH = pd.get_dummies(data, columns = ['Auction', 'Make', 'Color', 'VehYear', 
                                               'Nationality', 'Size', 'Body', 'TopThreeAmericanName', 
                                               'WheelType', 'Quater'])
     print("The number of features after one hot encoding is " + str(data_OH.shape[1]))
     
     """ Now we will start the numerical analysis """
-    # investigate all the numerical fields - start with WarrantyCost
-    print("WarrantyCost has zero: ",HasZero(data["WarrantyCost"]))
-    print("MMRAcquisitionAuctionAveragePrice has zero: ",HasZero(data["MMRAcquisitionAuctionAveragePrice"]))
-    print("MMRAcquisitionAuctionCleanPrice has zero: ",HasZero(data["MMRAcquisitionAuctionCleanPrice"]))
-    print("MMRRetailAveragePrice has zero: ",HasZero(data["MMRAcquisitionRetailAveragePrice"]))
-    print("MMRRetailCleanPrice has zero: ",HasZero(data["MMRAcquisitonRetailCleanPrice"]))
-    print("MMRCurrentAuctionCleanPrice has zero: ",HasZero(data["MMRCurrentAuctionAveragePrice"]))
-    print("MMRCurrentRetailAveragePrice has zero: ",HasZero(data["MMRCurrentAuctionCleanPrice"]))
-    print("MMRCurrentAuctionCleanPrice has zero: ",HasZero(data["MMRCurrentRetailAveragePrice"]))
-    print("MMRCurrentRetailCleanPrice has zero: ",HasZero(data["MMRCurrentRetailCleanPrice"]))
-    print("MMRCurrentRetailRatio has zero: ",HasZero(data["MMRCurrentRetailRatio"]))
-    
-    # [false, true, true, true, true, true, true, true, true, false]
     # so WarrantyCost and Ratios don't have null data
     # Foreach of the data fill in the mean
-    data["MMRAcquisitionAuctionAveragePrice"] = FillZeroWithMean(data["MMRAcquisitionAuctionAveragePrice"])
-    data["MMRAcquisitionAuctionCleanPrice"] = FillZeroWithMean(data["MMRAcquisitionAuctionCleanPrice"])
-    data["MMRAcquisitionRetailAveragePrice"] = FillZeroWithMean(data["MMRAcquisitionRetailAveragePrice"])
-    data["MMRAcquisitonRetailCleanPrice"] = FillZeroWithMean(data["MMRAcquisitonRetailCleanPrice"])
-    data["MMRCurrentAuctionAveragePrice"] = FillZeroWithMean(data["MMRCurrentAuctionAveragePrice"])
-    data["MMRCurrentAuctionCleanPrice"] = FillZeroWithMean(data["MMRCurrentAuctionCleanPrice"])
-    data["MMRCurrentRetailAveragePrice"] = FillZeroWithMean(data["MMRCurrentRetailAveragePrice"])
-    data["MMRCurrentRetailCleanPrice"] = FillZeroWithMean(data["MMRCurrentRetailCleanPrice"])
+    df["MMRAcquisitionAuctionAveragePrice"] = FillZeroWithMean(df["MMRAcquisitionAuctionAveragePrice"])
+    df["MMRAcquisitionAuctionCleanPrice"] = FillZeroWithMean(df["MMRAcquisitionAuctionCleanPrice"])
+    df["MMRAcquisitionRetailAveragePrice"] = FillZeroWithMean(df["MMRAcquisitionRetailAveragePrice"])
+    df["MMRAcquisitonRetailCleanPrice"] = FillZeroWithMean(df["MMRAcquisitonRetailCleanPrice"])
+    df["MMRCurrentAuctionAveragePrice"] = FillZeroWithMean(df["MMRCurrentAuctionAveragePrice"])
+    df["MMRCurrentAuctionCleanPrice"] = FillZeroWithMean(df["MMRCurrentAuctionCleanPrice"])
+    df["MMRCurrentRetailAveragePrice"] = FillZeroWithMean(df["MMRCurrentRetailAveragePrice"])
+    df["MMRCurrentRetailCleanPrice"] = FillZeroWithMean(df["MMRCurrentRetailCleanPrice"])
     
     # Test the correlations between Average and Clean
-    print (np.corrcoef(data["MMRAcquisitionAuctionAveragePrice"], data["MMRAcquisitionAuctionCleanPrice"] ))
-    print (np.corrcoef(data["MMRAcquisitionRetailAveragePrice"], data["MMRAcquisitonRetailCleanPrice"] ))
-    print (np.corrcoef(data["MMRCurrentAuctionAveragePrice"], data["MMRCurrentAuctionCleanPrice"] ))
-    print (np.corrcoef(data["MMRCurrentRetailAveragePrice"], data["MMRCurrentRetailCleanPrice"] ))  
+    print (np.corrcoef(df["MMRAcquisitionAuctionAveragePrice"], df["MMRAcquisitionAuctionCleanPrice"] ))
+    print (np.corrcoef(df["MMRAcquisitionRetailAveragePrice"], df["MMRAcquisitonRetailCleanPrice"] ))
+    print (np.corrcoef(df["MMRCurrentAuctionAveragePrice"], df["MMRCurrentAuctionCleanPrice"] ))
+    print (np.corrcoef(df["MMRCurrentRetailAveragePrice"], df["MMRCurrentRetailCleanPrice"] ))  
     
+    # We have high correlation so we drop the tables
     # Create new columns with the averaged amount due to high correlation
-    data["AcquisitionAuctionprice"] = CreateAveragedColumn(data["MMRAcquisitionAuctionAveragePrice"], data["MMRAcquisitionAuctionCleanPrice"])
-    data["AcquisitionRetailPrice"] = CreateAveragedColumn(data["MMRAcquisitionRetailAveragePrice"], data["MMRAcquisitonRetailCleanPrice"])
-    data["MMRCurrentAuctionPrice"] = CreateAveragedColumn(data["MMRCurrentAuctionAveragePrice"], data["MMRCurrentAuctionCleanPrice"])
-    data["MMRCurrentRetailPrice"] = CreateAveragedColumn(data["MMRCurrentRetailAveragePrice"], data["MMRCurrentRetailCleanPrice"])
+    df["AcquisitionAuctionprice"] = CreateAveragedColumn(df["MMRAcquisitionAuctionAveragePrice"], df["MMRAcquisitionAuctionCleanPrice"])
+    df["AcquisitionRetailPrice"] = CreateAveragedColumn(df["MMRAcquisitionRetailAveragePrice"], df["MMRAcquisitonRetailCleanPrice"])
+    df["MMRCurrentAuctionPrice"] = CreateAveragedColumn(df["MMRCurrentAuctionAveragePrice"], df["MMRCurrentAuctionCleanPrice"])
+    df["MMRCurrentRetailPrice"] = CreateAveragedColumn(df["MMRCurrentRetailAveragePrice"], df["MMRCurrentRetailCleanPrice"])
     
     # drop the other tables
-    data.drop(['MMRAcquisitionAuctionAveragePrice',
+    df.drop(['MMRAcquisitionAuctionAveragePrice',
                'MMRAcquisitionAuctionCleanPrice', 
                'MMRAcquisitionRetailAveragePrice', 
                'MMRAcquisitonRetailCleanPrice', 
@@ -216,11 +204,28 @@ def PreProcessing (df):
               axis=1, 
               inplace=True)
     
-    # Test correlation between Aquisition and Current
-    data_temp = data[['AcquisitionAuctionprice','AcquisitionRetailPrice', 'MMRCurrentAuctionPrice', 'MMRCurrentRetailPrice']]
-    print (np.corrcoef(data_temp)) # 90%
+    # Now do a box plots
+    df["AcquisitionAuctionprice"].hist()
+    df["AcquisitionRetailPrice"].hist()
+    df["MMRCurrentAuctionPrice"].hist()
+    df["MMRCurrentRetailPrice"].hist()
     
-    return data, data_OH
+    ## Identify and Remove outiers
+    outliers = (IdentifyOutliers(df["AcquisitionAuctionprice"]) and
+                IdentifyOutliers(df["AcquisitionRetailPrice"]) and
+                IdentifyOutliers(df["MMRCurrentAuctionPrice"]) and
+                IdentifyOutliers(df["MMRCurrentRetailPrice"]))
+    
+    # Select lines that aren't outliers
+    num_lines =  len(df["MMRCurrentRetailPrice"])
+    df = df[[not i for i in outliers]]
+    print ("Lines Removed: ", num_lines - len(df["MMRCurrentRetailPrice"]))
+                
+    # dealta (Q1, Q3) -> IQR
+    # lower min = Q1 - 1.5 * IQR
+    # higher value = Q3 + 1.5 * IQR
+	
+    return df, data_OH
 
 
 # Define a function to plot catgorical variables with relation to another cat, default is IsBadBuy
@@ -258,6 +263,23 @@ def CreateAveragedColumn (A, B):
         C[i] = (A[i] + B[i])/2
     
     return C
+
+def IdentifyOutliers (column):
+    data_described = column.describe()
+    Q1 = data_described["25%"]
+    Q3 = data_described["75%"]
+    IQR = Q3-Q1
+    lowerLimit = Q1 - 1.5 * IQR
+    upperLimit = Q3 + 1.5 * IQR
+    output = [None] * len( column )
+    for i in range(len(column)):
+        value = column[i]
+        if value < lowerLimit or value > upperLimit:
+            output[i] = True
+        else:
+            output[i] = False
+    return output
+            
 ### RUN THE OUTPUT ###
 data, data_OH = PreProcessing(data)
 Question1(data)
